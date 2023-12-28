befd9e78c0b163ebf04773e6d4f782ecbbd8adab
jiyaski <j.miyagi.hopkins@gmail.com>
2023-12-28 15:57:51 -0500

    using marked.js for markdown rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,46 @@
 
 import profilePhoto from '../assets/professional_portrait.jpg'
+import { marked } from 'marked'
 
 
 function Home() {
 
   console.log("Rendering Home component");
 
+  // cannot indent this string or it will be interpreted as code by Marked
+  var introText = `
+I’m Jonathan, a computer science student at Georgia Tech and aspiring software engineer. 
+
+I made this site partly to showcase my portfolio and partly on a whim. Maybe you can tell I got 
+nostalgic for 2010 when making this (the *Frutiger Aero* aesthetic sparks joy)! 
+
+<br>
+
+###### About me: 
+I used to study physics at the University of North Georgia. I originally intended to transfer 
+to Georgia Tech to study electrical engineering (via a dual-degree transfer program). However, 
+over time I found myself increasingly drawn to programming, so I eventually decided to transfer 
+as a computer science major instead. 
+
+So here I am, a busy jacket. Student by day, student by night. Student on the weekends. I also 
+like hiking, gardening, drawing, and reading old books ~~, not that I actually do much of those 
+things~~. One day I will have hobbies! But for now, programming and math are my life, and I'm 
+quite okay with that.
+  `
+
   return (
 
     <>
 
       {/* Welcome text */} 
-      <div className='my-8'></div>
+      <div className='my-6'></div>
       <div className='w-full text-center'>
         <h1 className='mx-auto text-6xl font-displayFont'>Welcome!</h1>
       </div>
-      <div className='my-6'></div>
+      <div className='my-4'></div>
 
       {/* main body text */} 
-      <div className='max-w-[800px] px-6'>
-        <p className='text-center'>I’m Jonathan, a computer science student at Georgia Tech and aspiring software engineer.</p>
-        <p className='text-center'>I originally studied physics at University of North Georgia, but the computers spoke to me. 
-            “C̸om͂e̴̽ t̚ǭ̷̦̹ t͚͖he̵̡̱͊̃ d͒ar͒k ̷̦͆ͅsi͝d̉e,” they said. “W̷͘e ̀há̴v̵͓̀e̴̾͜  ̛͕̈̐c̸̼̰̓̎̕ͅo̍͜o̫͎͝ki̊̓e̷͑s̵͕̍,” they said. I couldn’t refuse.</p>
-        <p className='text-center'>So here I am. Programmer by day, programmer by night. Math nerd on the weekends. 
-            Occasionally I like to hike in the mountains or read old books.</p>
-        <p className='text-center'>Anyway, feel free to look around. If you’re a potential employer, check out my résumé or see 
-            a list of projects I’ve worked on. My blog contains random thoughts I decided to write down 
-            - some applicable to programming and some not.</p>
-      </div>
+      <div className='max-w-[800px] px-6' dangerouslySetInnerHTML={{__html: marked.parse(introText)}}></div>
 
       {/* profile photo */} 
       <img src={profilePhoto} width="200px" alt="professional photo of me"
