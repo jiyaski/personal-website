6369f238fd879f8d842714a8c4ade89773a25eeb
jiyaski <j.miyagi.hopkins@gmail.com>
2024-01-08 22:47:24 -0500

    server endpoint fetches docs from mongodb

--- server/server.js
+++ server/server.js
@@ -4,20 +4,22 @@ const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 3000;
-const mongoURI = process.env.MONGO_URI; 
-const client = new MongoClient(mongoURI); 
 
+const mongoURI = process.env.MONGO_URI; 
+const dbName = 'personal-website'; 
+const client = new MongoClient(mongoURI); 
+let db; 
 
 async function connectToMongo() {
     try {
         await client.connect(); 
-        console.log("connected to mongodb"); 
+        console.log("connected to database"); 
     } catch (err) {
         console.error(err); 
     }
 }
 connectToMongo(); 
-
+db = client.db(dbName); 
 
 
 
@@ -25,9 +27,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/mongotest', (req, res) => {
-
+app.get('/projects', async (req, res) => {
+    try {
+        const collection = db.collection('projects'); 
+        const documents = await collection.find({}).toArray(); 
+        res.json(documents); 
+    } catch (err) {
+        res.status(500).send('Error fetching projects from database'); 
+    }
 })
+
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
