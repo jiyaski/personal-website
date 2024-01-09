e68960286545a5b50b88302979ea5cf62a5db1d5
jiyaski <j.miyagi.hopkins@gmail.com>
2024-01-08 22:04:58 -0500

    connect server to mongodb

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
+require('dotenv').config();  // loads environment variables from `.env` file into `process.env` object 
 const express = require('express');
+const { MongoClient } = require('mongodb'); 
+
 const app = express();
 const port = 3000;
+const mongoURI = process.env.MONGO_URI; 
+const client = new MongoClient(mongoURI); 
+
+
+async function connectToMongo() {
+    try {
+        await client.connect(); 
+        console.log("connected to mongodb"); 
+    } catch (err) {
+        console.error(err); 
+    }
+}
+connectToMongo(); 
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/mongotest', (req, res) => {
+
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
