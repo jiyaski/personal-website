1bc462fd0e5e4cc2dd315152bac4c20c0777eed2
jiyaski <j.miyagi.hopkins@gmail.com>
2024-01-15 01:23:48 -0500

    maybe /projects endpoint is working now?

---
+++ server/api/projects.js
@@ -0,0 +1,26 @@
+
+
+const { connectToMongo, applyCors } = require('../setup'); // Adjust the path as necessary
+
+
+module.exports = async (req, res) => {
+
+    applyCors(req, res);
+
+    // Only proceed for GET requests
+    if (req.method !== 'GET') {
+        res.setHeader('Allow', 'GET');
+        res.status(405).end('Method Not Allowed');
+        return;
+    }
+
+    try {
+        const db = await connectToMongo();
+        const collection = db.collection('projects');
+        const documents = await collection.find({}).toArray();
+        res.status(200).json(documents);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching projects from database');
+    }
+};
---
+++ server/pages/index.js
@@ -0,0 +1,4 @@
+
+module.exports = (req, res) => {
+    res.status(200).send("This project is only meant to host API routes, so there's nothing interesting here."); 
+}
